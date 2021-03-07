5be0557484ed5d3767313c2793aacb146c1c3fca
AbeLegend <abelegend23@gmail.com>
2021-03-07 09:38:45 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import CookieContainer from "./components/CookieContainer";
+
+function App() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }}>
+      <CookieContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CookieContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CookieContainer = () => {
+  return (
+    <div>
+      <h2>Number of cookies</h2>
+      <button>Buy Cookie</button>
+    </div>
+  );
+};
+
+export default CookieContainer;
