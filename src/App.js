85ac5eb41847491a5721df029a69b84c5b34db84
AbeLegend <abelegend23@gmail.com>
2021-03-07 19:15:30 +0700

    mapStateToProps

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ import CookieContainer from "./components/CookieContainer";
 import HooksCookieContainer from './components/HooksCookieContainer';
 import PuddingContainer from './components/PuddingContainer';
 import NewCookieContainer from './components/NewCookieContainer';
+import ItemContainer from './components/ItemContainer';
 
 function App() {
   return (
     <Provider store={store}>
+      <div style={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }}>
+        <ItemContainer cookie />
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }}>
+        <ItemContainer />
+      </div>
       <div style={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }}>
         <CookieContainer />
       </div>
